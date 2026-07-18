bdb97355be04de7a548ec3db15b48fa401a907a3
Hyunhee <h2koo2004@gmail.com>
2026-07-18 16:08:32 +0900

    충돌 해결 및 로그인 페이지 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,13 @@
 import "./globals.css";
 
 export const metadata = {
-<<<<<<< HEAD
   title: "LionKing",
   description: "LionKing project management service",
-=======
-  title: "Teamply",
-  description: "AI 협업 매니저 Teamply",
->>>>>>> origin/develop
 };
 
 export default function RootLayout({ children }) {
   return (
-<<<<<<< HEAD
-    <html
-      lang="ko"
-      className="h-full antialiased"
-    >
-=======
     <html lang="ko" className="h-full antialiased">
->>>>>>> origin/develop
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
